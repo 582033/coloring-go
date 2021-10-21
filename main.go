4c325c79d53ab6da242899ea4d429ac466f0e489
Yan Jiang <yan.jiang@marsmarch.cn>
2021-10-21 20:51:44 +0800

    fix

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ var id = flag.Int("id", 0, "要下载的coloring id")
 func main() {
 	flag.Parse()
 	if *id <= 0 {
-		fmt.Println("缺少参数id\n")
-		fmt.Println("使用-help查看\n")
+		fmt.Println("缺少参数id")
+		fmt.Println("使用-help查看")
 		return
 	}
 	coloringBookId := strconv.Itoa(*id)
